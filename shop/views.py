f09676d0c04434683de0313b743a8fd2bc0417bf
Sviatoslav Balema <sbalema2004@gmail.com>
2025-09-08 19:13:09 -0500

    fix: connect own login template instead of standard bootstrap login page

--- shop/views.py
+++ shop/views.py
@@ -44,17 +44,17 @@ def pricing(request):
 def account(request):
     if request.user.is_authenticated:
         return render(request, 'account/account_overview.html')
-    else:
-        if request.method == 'POST':
-            form = AuthenticationForm(request, data=request.POST)
-            if form.is_valid():
-                username = form.cleaned_data.get('username')
-                password = form.cleaned_data.get('password')
-                user = authenticate(request, username=username, password=password)
-                if user is not None:
-                    login(request, user)
-                    return redirect('account')
-        else:
-            form = AuthenticationForm()
 
-        return render(request, 'account/account_overview.html', {'form': form})
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('account')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'account/login.html', {'form': form})
